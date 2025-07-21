0c8b2b5936d14241e664bc3edd08f3443078cdfe
abhishek yadav <abhisathayadav143@gmail.com>
2025-07-21 06:04:46 +0530

    looger is added

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 import express from 'express'
 const app= express();
+import logger from "./logger.js";
+import morgan from "morgan";
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 const PORT=3000;
 
